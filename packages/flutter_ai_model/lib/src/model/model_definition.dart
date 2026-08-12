630182895b9f746ba86461e8d25eaf39e68ebd05
Jummery <lee.jummery@gmail.com>
2026-08-12 15:22:34 +0800

    feat: add model definition

---
+++ packages/flutter_ai_model/lib/src/model/model_definition.dart
@@ -0,0 +1,11 @@
+class ModelDefinition {
+  final String id;
+  final String name;
+  final String provider;
+
+  const ModelDefinition({
+    required this.id,
+    required this.name,
+    required this.provider,
+  });
+}
