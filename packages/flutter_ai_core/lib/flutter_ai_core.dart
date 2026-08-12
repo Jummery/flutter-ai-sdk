cbae50af72ca5928da3b44b2d589f5ee2b30de21
Jummery <lee.jummery@gmail.com>
2026-08-12 14:42:10 +0800

    feat: add flutter ai core exports

---
+++ packages/flutter_ai_core/lib/flutter_ai_core.dart
@@ -0,0 +1,7 @@
+library flutter_ai_core;
+
+export 'src/client/ai_client.dart';
+export 'src/config/ai_config.dart';
+export 'src/model/ai_model.dart';
+export 'src/message/chat_message.dart';
+export 'src/stream/ai_stream_event.dart';
