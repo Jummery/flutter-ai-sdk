0e3772addc33ca0da48bfb6b7624272526dcdf4e
Jummery <lee.jummery@gmail.com>
2026-08-13 11:37:04 +0800

    feat: wire storage database to drift tables

--- packages/flutter_ai_storage/lib/src/database/ai_database.dart
+++ packages/flutter_ai_storage/lib/src/database/ai_database.dart
@@ -1,5 +1,15 @@
-class AIDatabase {
-  const AIDatabase();
+import 'package:drift/drift.dart';
 
+import '../tables/conversations_table.dart';
+import '../tables/messages_table.dart';
+
+part 'ai_database.g.dart';
+
+/// Local Drift database for conversations and messages.
+@DriftDatabase(tables: [Conversations, Messages])
+class AIDatabase extends _$AIDatabase {
+  AIDatabase(super.e);
+
+  @override
   int get schemaVersion => 1;
 }
